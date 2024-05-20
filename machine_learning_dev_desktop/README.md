5be2d169108edafe91db8771ceb82c2578d44238
houbenzhou <houbenzhou@supermap.com>
2024-05-20 16:10:13 +0800

    机器学习开发环境桌面版

---
+++ machine_learning_dev_desktop/README.md
@@ -0,0 +1,10 @@
+## machine_learning_dev
+需要将最新版本的pycharm解压并重命名为pycharm，然后拷贝到software目录下
+
+## 编译镜像
+
+
+
+## 启动命令
+
+    
---
+++ machine_learning_dev_desktop/setconda.sh
@@ -0,0 +1,78 @@
+#  docker build  -f  Dockerfile_conda492_py38_ubuntu20 -t houbenzhou/conda:dockerfile_conda492_py38_ubuntu20_20210916  .
+
+FROM docker pull kasmweb/core-cuda-focal:1.15.0-rolling
+
+## 安装conda
+LABEL maintainer="Anaconda, Inc"
+
+ENV LANG=C.UTF-8 LC_ALL=C.UTF-8
+
+# hadolint ignore=DL3008
+RUN apt-get update -q && \
+    apt-get install -q -y --no-install-recommends \
+        bzip2 \
+        ca-certificates \
+        git \
+        libglib2.0-0 \
+        libsm6 \
+        libxext6 \
+        libxrender1 \
+        mercurial \
+        openssh-client \
+        procps \
+        subversion \
+        wget \
+        aria2c \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+ENV PATH /opt/conda/bin:$PATH
+
+CMD [ "/bin/bash" ]
+
+# Leave these args here to better use the Docker build cache
+ARG CONDA_VERSION=py312_24.3.0-0
+
+RUN set -x && \
+    UNAME_M="$(uname -m)" && \
+    if [ "${UNAME_M}" = "x86_64" ]; then \
+        MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-${CONDA_VERSION}-Linux-x86_64.sh"; \
+        SHA256SUM="00938c3534750a0e4069499baf8f4e6dc1c2e471c86a59caa0dd03f4a9269db6"; \
+    elif [ "${UNAME_M}" = "s390x" ]; then \
+        MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-${CONDA_VERSION}-Linux-s390x.sh"; \
+        SHA256SUM="a150511e7fd19d07b770f278fb5dd2df4bc24a8f55f06d6274774f209a36c766"; \
+    elif [ "${UNAME_M}" = "aarch64" ]; then \
+        MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-${CONDA_VERSION}-Linux-aarch64.sh"; \
+        SHA256SUM="48a96df9ff56f7421b6dd7f9f71d548023847ba918c3826059918c08326c2017"; \
+    elif [ "${UNAME_M}" = "ppc64le" ]; then \
+        MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-${CONDA_VERSION}-Linux-ppc64le.sh"; \
+        SHA256SUM="4c86c3383bb27b44f7059336c3a46c34922df42824577b93eadecefbf7423836"; \
+    fi && \
+    wget "${MINICONDA_URL}" -O miniconda.sh -q && \
+    echo "${SHA256SUM} miniconda.sh" > shasum && \
+    if [ "${CONDA_VERSION}" != "latest" ]; then sha256sum --check --status shasum; fi && \
+    mkdir -p /opt && \
+    bash miniconda.sh -b -p /opt/conda && \
+    rm miniconda.sh shasum && \
+    ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
+    echo "conda activate base" >> ~/.bashrc && \
+    find /opt/conda/ -follow -type f -name '*.a' -delete && \
+    find /opt/conda/ -follow -type f -name '*.js.map' -delete && \
+    /opt/conda/bin/conda clean -afy
+
+USER root
+ENV SOFTWARE=/root/software
+RUN mkdir $SOFTWARE
+COPY software $SOFTWARE/
+
+RUN rm -rf /root/.pip/ && \
+    mkdir -p /root/.pip && \
+    mkdir -p /root/.jupyter && \
+    cp -r $SOFTWARE/pip.conf  /root/.pip/ && \
+    cp $SOFTWARE/jupyter_lab_config.py  /root/.jupyter/  &&\
+    cp $SOFTWARE/jupyter_server_config.json  /root/.jupyter/ && \
+    cp $SOFTWARE/pycharm.desktop  /usr/share/applications
+
+
+
